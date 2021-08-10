81c610c26bc1fd12b3287ea3ed3a135619ccadb4
Catherine Cao <catherine.cao@ibm.com>
2021-08-10 18:57:44 +0000

    delete temp files

--- assets/jupyterlab/untitled1.py
+++
@@ -1,5 +0,0 @@
-import sys
-if len(sys.argv) > 0:
-    print(sys.argv)
-else:
-    print("no argv")
--- assets/jupyterlab/utility_test.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.insert(0,"/project_data/data_asset")
-import cpd_software_env
-cpd_software_env.setup("batch_hello_env.yaml")
-
